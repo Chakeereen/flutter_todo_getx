2a7a62f514b260d94703f61d50613861c40d20d3
Chakeereen <65143052@g.cmru.ac.th>
2025-03-12 16:22:14 +0700

    init

--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,20 +1,5 @@
 import 'package:get_storage/get_storage.dart';
 
-// class StorageService {
-//   final box = GetStorage();
-
-//   Future<void> init() async {
-//     await GetStorage.init();
-//   }
-
-//   Future<void> write(String key, dynamic value) async {
-//     await box.write(key, value);
-//   }
-
-//   dynamic read(String key) {
-//     return  box.read(key);
-//   } 
-// }
 class StorageService {
   final box = GetStorage();
   Future<void> init() async {
